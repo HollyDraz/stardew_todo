aa6c15408bf48a4139e8ac4be0d9f748edc8fd6a
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-03-12 14:59:18 -0500

    cleaned up js

--- public/scripts.js
+++ public/scripts.js
@@ -1,45 +1,37 @@
-
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+// Load existing items from localStorage when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  loadItems();
+});
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
+    saveItems(); // Save state when toggling checked
   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+    return;
   }
+
+  addItem(inputValue, false);
+  saveItems();
   document.getElementById("myInput").value = "";
+}
+
+function addItem(value, isChecked) {
+  var li = document.createElement("li");
+  li.textContent = value;
+
+  if (isChecked) {
+    li.classList.add('checked');
+  }
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -47,10 +39,29 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  document.getElementById("myUL").appendChild(li);
+
+  // Close button event to remove item
+  span.onclick = function() {
+    li.remove();
+    saveItems();
+  };
+}
+
+// Save list items and their state to localStorage
+function saveItems() {
+  var listItems = [];
+  document.querySelectorAll("#myUL li").forEach(li => {
+    listItems.push({
+      value: li.textContent.replace("\u00D7", "").trim(),
+      isChecked: li.classList.contains('checked')
+    });
+  });
+  localStorage.setItem("items", JSON.stringify(listItems));
+}
+
+// Load list items from localStorage
+function loadItems() {
+  var items = JSON.parse(localStorage.getItem("items")) || [];
+  items.forEach(item => addItem(item.value, item.isChecked));
 }
